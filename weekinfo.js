b333ec36f93a4f94e5e515c70276f8c0f8147bfc
lutas <phil@lutas.co.uk>
2017-07-22 19:04:22 +0100

    Fixed calculation of date ranges from Monday to Sunday

--- weekinfo.js
+++ weekinfo.js
@@ -2,15 +2,15 @@ module.exports = function(date) {
 
     let curDay = date.getDay();
     // move from JS 0=sunday, to 0=monday 
-    let daysFromMonday = (curDay - 1) % 7;
+    let daysFromMonday = ((curDay - 1) % 7);
 
-    let monday = new Date(date.getFullYear(), date.getMonth(), date.getDay() - daysFromMonday);
-    let sunday = new Date(date.getFullYear(), date.getMonth(), date.getDay() + (7 - daysFromMonday));
+    let monday = new Date(date.getFullYear(), date.getMonth(), date.getDate() - daysFromMonday, 12);
+    let sunday = new Date(date.getFullYear(), date.getMonth(), date.getDate() + (6 - daysFromMonday), 12);
 
     // work out date info
     return {
         date: date,
-        todayNum: daysFromMonday,
+        todayNum: daysFromMonday + 1,
         start: monday,
         end: sunday
     }
